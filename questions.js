380a4e3ebecb1e021fd417b250e56562931972b8
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-09-02 12:44:24 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"A: Can you play _____ violin? - B: Yes, and _____ my favourite musical instrument.",
+        options:["the / it is", "a / this is", "— / that is", "an / —"],
+        answer: 0
+    },
+    {
+        q:"In general, _____ English love tea and _____ French love wine.",
+        options:["— / —", "the / the", "an / a", "a / an"],
+        answer: 1
+    },
+    {
+        q:"A: Do you play _____ tennis? - B: No, but I think it is _____ good sport.",
+        options:["a / the", "the / —", "— / a", "a / —"],
+        answer: 2
+    },
+    {
+        q:"_____ bees are very active animals.",
+        options:["An", "The", "A", "—"],
+        answer: 3
+    },
+    {
+        q:"My parents are on the phone. They are in _____ Italy, near _____ Leaning Tower of Pisa.",
+        options:["— / the", "an / a", "a / —", "the / the"],
+        answer: 0
+    },
+    {
+        q:"_____ Simpsons bought a new house by _____ Mediterranean Sea.",
+        options:["A / a", "The / the", "— / the", "— / —"],
+        answer: 1
+    },
+    {
+        q:"We will have _____ lunch at a restaurant by _____ Nile.",
+        options:["an / the", "a / —", "— / the", "some / a"],
+        answer: 2
+    },
+    {
+        q:"A: Is your dentist appointment in _____ morning? - B: Yes. I have an appointment with _____ Merry Young.",
+        options:["some / the", "— / a", "a / the", "the / —"],
+        answer: 3
+    },
+    {
+        q:"They live in _____ Philippines and work in _____ hospital",
+        options:["the / a", "the / an", "— / the", "— / —"],
+        answer: 0
+    },
+    {
+        q:"_____ Bermuda Triangle is in _____ Atlantic Ocean.",
+        options:["A / an", "The / the", "The / a", "— / —"],
+        answer: 1
+    }
+
+
+
+]
